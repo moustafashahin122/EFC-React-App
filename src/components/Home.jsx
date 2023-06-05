2d8fa6e290e5864a858826c4c8565b5dd78506a8
abdallah <abdallah011848@gmail.com>
2023-06-06 02:10:42 +0300

    m

---
+++ src/components/Home.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Card from "./Cards";
+import Slide1 from "./slider1";
+import Card_Item from "./Card item";
+import Navbar from "./Slider";
+import Slider from "./Slider";
+
+const Home = () => {
+
+
+  return (
+    <>
+     <Slider/>
+     <div className="f">
+     <Card_Item />
+     <Card_Item />
+     <Card_Item />
+     <Card_Item />
+     </div>
+      <Slider />
+    </>
+  );
+};
+
+export default Home;
+
+ 
---
+++ src/redux/slices/ProductsSlice.js
@@ -0,0 +1,34 @@
+// import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+// import axios from "axios";
+
+// const initialState = {
+//   Products: [],
+//   full: false,
+// };
+
+// export const getAllProducts = createAsyncThunk("getAllProducts", async () => {
+//   const { data } = await axios.get("https://fakestoreapi.com/products");
+//   console.log(data);
+//   return data;
+// });
+// const CartSlice = createSlice({
+//   name: "Products",
+//   initialState,
+//   reducers: {
+//     // fectchProducts: (state, action) => {
+//     //   state.Products = action.payload;
+//     // },
+//   },
+
+//   extraReducers: (builder) => {
+//     builder.addCase(getAllProducts.fulfilled, (state, action) => {
+//       state.Products = action.payload;
+//       console.log(action.payload);
+//       state.full = true;
+//     });
+//   },
+// });
+
+// export const { addToCart, removeFromCart } = CartSlice.actions;
+
+// export default CartSlice.reducer;
